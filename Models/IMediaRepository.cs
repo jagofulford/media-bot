b9dd1844c6d301b0d7aec6c1314464c9dade028b
Jago Fulford <jago.fulford@gmail.com>
2021-03-10 11:18:35 +0000

    Creation of media object to encapsulate media types (Tv. film etc) and a repository interfact to interact with external sources

---
+++ Models/IMediaRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace media_bot.Models
+{
+    interface IMediaRepository
+    {
+        IEnumerable<MediaObject> SearchMedia(string searchTerm);
+        bool AddMedia(string uniqueId);
+    }
+}
---
+++ Models/MediaObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace media_bot.Models
+{
+    enum Source
+    {
+        sonarr,
+        radarr
+    }
+    class MediaObject
+    {
+        readonly string title;
+        readonly int year;
+        readonly string poster;
+        readonly string overview;
+        readonly string id;
+        readonly Source source;
+
+        MediaObject(string title, int year, string poster, string overview, string id, Source source)
+        {
+            this.title = title;
+            this.year = year;
+            this.poster = poster;
+            this.overview = overview;
+            this.id = id;
+            this.source = source;
+        }
+
+        public string GetMediaDisplayTitle()
+        {
+            var year = (this.year == 0) ? "unk" : this.year.ToString();
+            return this.title + " (" + year + ") " + this.overview.Substring(0,100);
+        }
+    }
+}
